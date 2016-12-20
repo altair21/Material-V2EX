ff9aa23a090fb9bfd7a43ee5581c606db842315a
Bohao Chen <bohaoc@curacloudcorp.com>
2016-12-20 18:57:43 +0800

    初始化

---
+++ Material-V2EX/Material-V2EX/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Material-V2EX
+//
+//  Created by altair21 on 16/12/20.
+//  Copyright © 2016年 altair21. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
