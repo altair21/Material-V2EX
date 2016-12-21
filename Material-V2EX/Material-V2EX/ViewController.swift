e333613806beee963b480085a2561e297911f8e2
Bohao Chen <bohaoc@curacloudcorp.com>
2016-12-21 17:18:01 +0800

    首页 导航栏 UI完成

--- Material-V2EX/Material-V2EX/ViewController.swift
+++ Material-V2EX/Material-V2EX/ViewController.swift
@@ -7,12 +7,38 @@
 //
 
 import UIKit
+import Material
+import AMScrollingNavbar
 
 class ViewController: UIViewController {
+    @IBOutlet weak var postBtn: FabButton!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var menuBtn: FabButton!
+    @IBOutlet weak var moreBtn: FabButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let navigationController = navigationController as? ScrollingNavigationController {
+            navigationController.followScrollView(tableView, delay: 40.0)
+            navigationController.navigationBar.topItem!.title = "111"
+            let leftButton = IconButton(image: Icon.cm.menu)
+            navigationItem.title = "test"
+            navigationController.navigationBar.topItem!.leftViews = [leftButton]
+        }
+        prepareMaterial()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        if let navigationController = navigationController as? ScrollingNavigationController {
+            navigationController.followScrollView(tableView, delay: 40.0)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +46,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func prepareMaterial() {
+        postBtn.image = Icon.add
+        menuBtn.image = Icon.cm.menu
+        moreBtn.image = Icon.cm.moreHorizontal
+//        menuBtn.backgroundColor = UIColor.clear
+        
+    }
+    
+    @IBAction func menuTapped(_ sender: FabButton) {
+    }
+    
+    @IBAction func moreTapped(_ sender: FabButton) {
+    }
+    
+}
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "postOverviewCell")!
+        return cell
+    }
+}
+
+extension ViewController: ScrollingNavigationControllerDelegate {
+    func scrollViewShouldScrollToTop(_ scrollView: UIScrollView) -> Bool {
+        if let navigationController = navigationController as? ScrollingNavigationController {
+            navigationController.showNavbar(animated: true)
+        }
+        return true
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if let navigationController = navigationController {
+            let oldY = tableView.frame.origin.y
+            let newY = navigationController.navigationBar.frame.height + navigationController.navigationBar.frame.origin.y
+            tableView.frame.origin.y = newY
+            tableView.frame.size.height += oldY - newY
+        }
+    }
 }
 
